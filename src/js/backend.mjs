265055964ddf25d102664a84e1f12e2e08747653
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2026-01-30 14:26:24 +0100

    pdimages et affichage des infos

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -1,10 +1,10 @@
 import PocketBase from 'pocketbase';
 
-const db = new PocketBase(/* URL serveur pocketbase */);
+const db = new PocketBase('http://127.0.0.1:8090');
 
 export async function getOffres() {
     try {
-        let data = await db.collection('maison').getFullList({
+        let data = await db.collection('maisons').getFullList({
             sort: '-created',
         });
         return data;
