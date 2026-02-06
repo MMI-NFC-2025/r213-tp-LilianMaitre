8cdfe284c7a5ecea09ec32fd7ba94926c6153204
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2026-02-06 13:48:14 +0100

    ajout "voir plus"

--- backend.mjs
+++ backend.mjs
@@ -134,3 +134,13 @@ export async function maisonfavoriagent(favori, agentId) {
     }
 }
 
+export async function getOffre(id) {
+    try {
+        const data = await db.collection('maison').getOne(id);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return null;
+    }
+}
+
